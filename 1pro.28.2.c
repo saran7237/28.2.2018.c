8ddffb13f195100225dc02a17e8bc43c80208ebf
saran7237 <36067371+saran7237@users.noreply.github.com>
2018-02-28 15:48:06 +0530

    Create 1pro.28.2.c

---
+++ 1pro.28.2.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<cono.h>
+int main ()
+{
+int n;
+printf("enter the numbers");
+for(i=1;i<=1;i++)
+printf("enter the even numbers");
+scanf("%d",&a);
+c=a+b;
+printf("enter the total values");
+scanf("%d",&c);
+getch();
+return 0;
+}
