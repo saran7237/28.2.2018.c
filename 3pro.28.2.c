93c8801fae71d0377a7996941a1e727beaa512b2
saran7237 <36067371+saran7237@users.noreply.github.com>
2018-02-28 16:01:08 +0530

    Create 3pro.28.2.c

---
+++ 3pro.28.2.c
@@ -0,0 +1,18 @@
+import java.util.*;
+import java.io.*;
+public class Prgm32
+{
+	public static void main(String s[])
+	{
+		int c=0;
+		String a;
+		Scanner sc=new Scanner(System.in);
+		a=sc.nextLine();
+		String aa=a.split("\\sc+");
+		for(i=0;i<aa.length;i++)
+		{
+			c;
+      }
+      }
+		
+	
